6b69e1f045857c5c3b5b979311a8d29d0da39351
Eduard Bozhko <bozhko@twnty.de>
2021-08-27 16:43:01 +0300

    added preloader component

---
+++ components/Preloader/Preloader.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from "react";
+import { PreloaderContainer } from "./Preloader.styles";
+
+const Preloader:FC = ()=>{
+    return (
+    <PreloaderContainer>
+    </PreloaderContainer>
+    )
+}
+export default Preloader
